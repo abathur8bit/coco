dae8dc24f406a77ee1add8d243456f2376619fdf
Abathur8bit <abathur8bit@8bitcoder.com>
2018-03-12 15:18:36 -0400

    updates to display minefield and player

--- asm/robot2.asm
+++ asm/robot2.asm
@@ -1,5 +1,6 @@
+* lwasm -3 -b -o robot2.bin robot2.asm && writecocofile -b robot.dsk robot2.bin && coco3 robot.dsk robot2
 * 8 character tabs
-* This program comes from the Robot Mines game found in Tim Hartnell's Giant Book of Computer Games.
+* This program comes from the Robot Minefield game found in Tim Hartnell's Giant Book of Computer Games.
 * I am converting it to assembly as a learning excersize.
 *
 * http://8BitCoder.com
@@ -8,41 +9,112 @@
             	org	$0e00
 
 start
-		jsr	[cbcls]
+		jsr	clearscreen
+		jsr	showtitle
+		jsr	wait
 		
+
+game
+		jsr	clearminefield		
+		
+gameloop	jsr	drawminefield
+		jsr	gethumanmove
+		lda	endgameflag		;game end?
+		beq	gameloop		;loop if not (endgame=0)
+		
+		jsr	clearscreen
+		ldx	#thanks
+		jsr	print
+		rts
+
+
+
+clearminefield	lda	#46+64
+		ldb	#12*12
+		ldx	#minefield
+a@		sta	,x+
+		decb
+		bne	a@
+		lda	#human			;place player
+		sta	minefield
+		rts
+
+
+
+drawminefield	ldu	#$400+32		;third line down
+		ldx	#minefield		
+		ldd	#$0c0c
+		std	xpos			;x&y pos loop counters
+a@		lda	,x+
+		sta	,u+
+		dec	xpos
+		bne	a@
+		lda	#12
+		sta	xpos
+		leau	32-12,u			;point to next line
+		dec	ypos			;dec counter
+		bne	a@			;loop if not 0
+		rts
+		
+		
+		
+gethumanmove	jsr	wait
+		rts
+		
+		
+			
+showtitle				
 		ldx	#title
 		jsr	print
 		ldx	#blankline
 		jsr	print
+		ldx	#agameby
+		jsr	print
+		ldx	#blankline
+		jsr	print
 		ldx	#originalauthor
-		jsr	print
-		
-		ldd	#$0101
-		jsr	setcursorxy
-;		ldu	#$400
-;		ldd	#32*8
-;		leau	d,u
-;		stu	cbcurpos
-
-		ldx	#done
-		jsr	print
-		
+		jsr	print	
+		jsr 	wait
 		rts
 		
 		
+		
+		
 *			"                                "	32 columns
-title		fcc	"        ROBOT MINEFIELD"
 blankline    	fcb	13,0
+title		fcc	"        ROBOT MINEFIELD"
+	    	fcb	13,0
+agameby		fcc	"    A GAME BY LEE PATTERSON"
+	    	fcb	13,0
 originalauthor	fcc	"ADAPTED FROM TIM HARTNELL'S"
 		fcb	13
 		fcc	"GIANT BOOK OF COMPUTER GAMES"
 	    	fcb	13,0
-
+thanks		fcc	"THANKS FOR PLAYING"
+		fcb	13
+		fcc	"ROBOT MINEFIELD"
+		fcb	13
+		fcc	"WWW.8BITCODER.COM"
+	    	fcb	13,0
 helloworld	fcc	"HELLO WORLD"
 	    	fcb	13,0
 done		fcc	"DONE"
 		fcb	13,0
 		
+human		equ	'H'
+humanpos	fcb	0		; center of the screen
+robot		equ	'$'
+mine		equ	'*'
+
+field_width	equ	12
+field_height	equ	12
+minefield	rmb	field_width*field_height
+minefieldend	fcb	0
+
+endgameflag	fcb	0
+
+xpos		fcb	0
+ypos		fcb	0
 		include	text.asm
 	
 		end	start
