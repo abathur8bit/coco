0cd693d94625bc93adfc5b820be90af304d6f55e
Abathur8bit <abathur8bit@8bitcoder.com>
2019-02-09 11:14:54 -0500

    tabs to spaces

--- hmode/mainml.asm
+++ hmode/mainml.asm
@@ -5,45 +5,45 @@ INIT0_REG       equ     $ff90
 VMODE_REG       equ     $ff98
 VRES_REG        equ     $FF99
 
-                include     	'hmode256.inc'
+                include         'hmode256.inc'
 
-                org         	$2800
+                org             $2800
 
-                section     	code
+                section         code
 
-start           sta         	$ffd9
-                lbsr        	_initGraphics
+start           sta             $ffd9
+                lbsr            _initGraphics
 
-	
-	; clear screen
-	ldd	#$FFFF
-	pshs	d
-	lbsr	_clearScreen
-	puls	d
+                
+                ; clear screen
+                ldd             #$FFFF
+                pshs            d
+                lbsr            _clearScreen
+                puls            d
 
-	setmmupage2
-	
-	; clear screen
-	ldd	#0
-	pshs	d
-	lbsr	_clearScreen
-	puls	d
+                setmmupage2
+                
+                ; clear screen
+                ldd             #0
+                pshs            d
+                lbsr            _clearScreen
+                puls            d
 
-                setpixel    	#96,#60,#2
-	setpixel	#128,#96,#2
-	
-	setmmupage1
-	setpixel	#10,#10,#10
-	
-l1	
-	setpage2
-	setpage1
-	jmp	l1
-	
-endless         jmp         	endless
+                setpixel        #96,#60,#2
+                setpixel        #128,#96,#2
+                
+                setmmupage1
+                setpixel        #10,#10,#10
+                
+l1              
+                setpage2
+                setpage1
+                jmp             l1
+                
+endless         jmp             endless
 
                 rts
 
                 endsection
 
-                end         	start
+                end             start
