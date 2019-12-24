42422d20b0e281dfd6f57df84bed82c15545229d
Abathur8bit <Abathur8bit@8bitcoder.com>
2019-12-24 06:40:46 -0500

    Additional color states, score implemented, and game end is detected.

--- memory/memory.c
+++ memory/memory.c
@@ -39,9 +39,13 @@
 
 #define COLOR_CARD_TOP                      1
 #define COLOR_CARD_UNDERSIDE                2
-#define COLOR_CARD_SELECTED                 3
-#define COLOR_CARD_UNDERSIDE_SELECTED       4
+#define COLOR_CARD_TOP_CURSOR               3
+#define COLOR_CARD_UNDERSIDE_CURSOR         4
 #define COLOR_NORMAL                        5
+#define COLOR_MESSAGE                       6
+#define COLOR_CARD_UNDERSIDE_NOCURSOR       7
+
+#define OFFSET_TOP                          2   //where to start drawing text
 
 typedef struct {
     char value;
@@ -49,28 +53,15 @@ typedef struct {
     BOOL flipped;
 } CARD;
 
-CARD cards[56];
+#define ROW_CARDS 13
+CARD cards[52];
 CARD* selectedCard = NULL;  //NULL non selected
-CARD* firstCard = NULL;
-CARD* secondCard = NULL;
+CARD* firstCard = NULL;  //NULL non selected
+CARD* secondCard = NULL;  //NULL non selected
 char buffer[80*25];
 BOOL playing = TRUE;
 int score = 0;
-
-void initCards() {
-    char suites[] = "CSDH";
-    char cardValues[] = "123456789TJQKA";
-
-    int cardIndex = 0;
-    for (int s = 0; s < sizeof(suites)-1; ++s) {
-        for (int c = 0; c < sizeof(cardValues)-1; ++c) {
-            cards[cardIndex].value = cardValues[c];
-            cards[cardIndex].suite = suites[s];
-            cards[cardIndex].flipped = FALSE;
-            ++cardIndex;
-        }
-    }
-}
+int best = -1;
 
 /**
  * Returns a number between min and max inclusive.
@@ -82,13 +73,49 @@ int rnd(int min, int max) {
     return n;
 }
 
+void shuffleCards() {
+    CARD temp;
+    int a, b;
+    for (int i = 0; i < 100; i++) {
+        a = rnd(0, 51);
+        b = rnd(0, 51);
+        temp = cards[a];
+        cards[a] = cards[b];
+        cards[b] = temp;
+    }
+}
+
+void initCards() {
+    char suites[] = "CSDH";
+    char cardValues[] = "23456789TJQKA";
+
+    int cardIndex = 0;
+    for (int s = 0; s < sizeof(suites)-1; ++s) {
+        for (int c = 0; c < sizeof(cardValues)-1; ++c) {
+            if (cardIndex > sizeof(cards) / sizeof(cards[0])) {
+                deinitSystem();
+                printf("Too many cards created in initCards\n");
+                exit(1);
+            }
+            cards[cardIndex].value = cardValues[c];
+            cards[cardIndex].suite = suites[s];
+            cards[cardIndex].flipped = FALSE;
+            ++cardIndex;
+        }
+    }
+
+    shuffleCards();
+}
+
 void setupColorPairs() {
 #ifndef _COCO_BASIC_
-    init_pair(COLOR_NORMAL, COLOR_WHITE, COLOR_BLACK);
     init_pair(COLOR_CARD_TOP, COLOR_YELLOW, COLOR_RED);
     init_pair(COLOR_CARD_UNDERSIDE, COLOR_BLACK, COLOR_YELLOW);
-    init_pair(COLOR_CARD_SELECTED, COLOR_BLACK, COLOR_CYAN);
-    init_pair(COLOR_CARD_UNDERSIDE_SELECTED, COLOR_WHITE, COLOR_CYAN);
+    init_pair(COLOR_CARD_TOP_CURSOR, COLOR_WHITE, COLOR_CYAN);
+    init_pair(COLOR_CARD_UNDERSIDE_CURSOR, COLOR_WHITE, COLOR_CYAN);
+    init_pair(COLOR_CARD_UNDERSIDE_NOCURSOR, COLOR_BLACK, COLOR_CYAN);
+    init_pair(COLOR_NORMAL, COLOR_WHITE, COLOR_BLACK);
+    init_pair(COLOR_MESSAGE, COLOR_BLACK, COLOR_CYAN);
 #endif // !_COCO_BASIC_
 
 }
@@ -102,14 +129,20 @@ void colorPair(byte pair) {
     case COLOR_CARD_UNDERSIDE:
         setColor(COLOR_BLACK, COLOR_YELLOW);
         break;
-    case COLOR_CARD_SELECTED:
-        setColor(COLOR_BLACK, COLOR_CYAN);
-        break;
-    case COLOR_CARD_UNDERSIDE_SELECTED:
+    case COLOR_CARD_TOP_CURSOR:
         setColor(COLOR_WHITE, COLOR_CYAN);
+        break;
+    case COLOR_CARD_UNDERSIDE_CURSOR:
+        setColor(COLOR_WHITE, COLOR_CYAN);
+        break;
+    case COLOR_CARD_UNDERSIDE_NOCURSOR:
+        setColor(COLOR_BLACK, COLOR_CYAN);
         break;
     case COLOR_NORMAL:
         setColor(COLOR_WHITE, COLOR_BLACK);
+        break;
+    case COLOR_MESSAGE:
+        setColor(COLOR_BLACK, COLOR_CYAN);
         break;
 }
 #else 
@@ -120,36 +153,52 @@ void colorPair(byte pair) {
 void drawCard(byte x,byte y,CARD* c) {
     char spaces[] = "    ";
     char buff[]   = " <> ";
-    if (c == selectedCard && c->flipped) {
-        colorPair(COLOR_CARD_UNDERSIDE_SELECTED);
-        buff[1] = c->value;
-        buff[2] = c->suite;
-    } else if (c == selectedCard) {
-        colorPair(COLOR_CARD_SELECTED);
-    } else if (c->flipped) {
-        buff[1] = c->value;
-        buff[2] = c->suite;
-        colorPair(COLOR_CARD_UNDERSIDE);
+    //not flipped                       COLOR_CARD_TOP
+    //flipped                           COLOR_CARD_UNDERSIDE
+    //flipped current turn              COLOR_CARD_UNDERSIDE_NOCURSOR
+    //not flipped and cursor on it      COLOR_CARD_TOP_CURSOR
+    //flipped and cursor on it          COLOR_CARD_UNDERSIDE_CURSOR
+
+    if (c == selectedCard) {
+        if (c->flipped) {
+            buff[1] = c->value;
+            buff[2] = c->suite;
+            colorPair(COLOR_CARD_UNDERSIDE_CURSOR);
+        }
+        else {
+            colorPair(COLOR_CARD_TOP_CURSOR);
+        }
     }
     else {
-        colorPair(COLOR_CARD_TOP);
+        if (c->flipped) {
+            buff[1] = c->value;
+            buff[2] = c->suite;
+            if (c == firstCard || c == secondCard) {
+                colorPair(COLOR_CARD_UNDERSIDE_NOCURSOR);
+            }
+            else {
+                colorPair(COLOR_CARD_UNDERSIDE);
+            }
+        }
+        else {
+            colorPair(COLOR_CARD_TOP);
+        }
     }
-    //setInverseText();
+
     gotoxy(x, y);
     textout(spaces);
     gotoxy(x, y+1);
     textout(buff);
     gotoxy(x, y+2);
     textout(spaces);
-    //setNormalText();
 }
 
 void drawDeck() {
     int cardIndex = 0;
-    byte offsetx = 5;
-    byte y = 3;
+    byte offsetx = SCREEN_WIDTH/2-(ROW_CARDS*5/2);
+    byte y = OFFSET_TOP+4;
     while (cardIndex < 52) {
-        for (int x = 0; x < 14; ++x) {
+        for (int x = 0; x < ROW_CARDS; ++x) {
             //gotoxy((byte)(offsetx + x * 3), y);
             //char buffer[] = "  ";
             //buffer[0] = cards[cardIndex].value;
@@ -176,12 +225,39 @@ void drawField() {
 
 void showMessage(const char* s) {
 
-    colorPair(COLOR_CARD_UNDERSIDE_SELECTED);
+    colorPair(COLOR_MESSAGE);
     textoutxy(0, 23, "                                                                                ");
     centertext(23, s);
     if (waitforkey() == ESCAPE) playing = FALSE;
     colorPair(COLOR_NORMAL);
     textoutxy(0, 23,"                                                                                ");
+}
+
+void drawScore() {
+    colorPair(COLOR_NORMAL);
+    //                        1         2         3         4         5         6         7         8
+    //               12345678901234567890123456789012345678901234567890123456789012345678901234567890
+    char bestShown[] = "----";
+    if (best != -1) {
+        sprintf(bestShown, "%04d", best);
+    }
+    sprintf(buffer, "TURNS: %04d                                           BEST: %4s", score, bestShown);
+    centertext(OFFSET_TOP+2, buffer);
+}
+
+void fakeWin() {
+    for (int i = 0; i < 52; ++i) {
+        cards[i].flipped = TRUE;
+    }
+}
+
+BOOL checkWin() {
+    int flipped = 0;
+    for (int i = 0; i < 52; ++i) {
+        if(cards[i].flipped)
+            flipped++;
+    }
+    return 52 == flipped;
 }
 
 void playGame() {
@@ -196,9 +272,8 @@ void playGame() {
     BOOL isCardChosen = FALSE;
     while (playing) {
         colorPair(COLOR_NORMAL);
-        centertext(0, "M E M O R Y");
-        sprintf(buffer, "SCORE: %05d", score);
-        textoutxy(0, 1, buffer);
+        centertext(OFFSET_TOP, "M E M O R Y");
+        drawScore();
         drawDeck();
         int ch = waitforkey();
         switch (ch) {
@@ -222,25 +297,27 @@ void playGame() {
             break;
         }
 
-        if (selectx < 0) selectx = 13;
-        if (selectx > 13) selectx = 0;
+        if (selectx < 0) selectx = ROW_CARDS-1;
+        if (selectx > ROW_CARDS-1) selectx = 0;
         if (selecty < 0) selecty = 3;
         if (selecty > 3) selecty = 0;
 
-        selectedCard = &cards[selecty * 14 + selectx];
+        selectedCard = &cards[selecty * ROW_CARDS + selectx];
         if (isCardChosen) {
             isCardChosen = FALSE;   //reset
             if (!firstCard) {
                 firstCard = selectedCard;
                 firstCard->flipped = TRUE;
             }
-            else {
+            else if(firstCard != selectedCard) {
                 secondCard = selectedCard;
                 secondCard->flipped = TRUE;
                 drawDeck();
             }
 
             if (firstCard && secondCard) {
+                ++score;
+                drawScore();
                 BOOL match = FALSE;
                 char* msg[2] = { "Cards don't match","Match!" };
                 if (firstCard->value == secondCard->value) {
@@ -249,14 +326,33 @@ void playGame() {
                 else {
                     match = FALSE;
                 }
-                showMessage(msg[match]);
 
                 if (!match) {
                     firstCard->flipped = FALSE;
                     secondCard->flipped = FALSE;
                 }
+                showMessage(msg[match]);
                 firstCard = secondCard = NULL;
             }
+        }
+
+        if (ch == 'W') {
+            fakeWin();
+            drawDeck();
+        }
+
+        if (checkWin()) {
+            if(score < best || best == -1) {
+                best = score;
+                drawScore();
+                sprintf(buffer, "You won in %d turns. New best!", score);
+            }
+            else {
+                sprintf(buffer, "You won in %d turns.", score);
+            }
+            showMessage(buffer);
+            score = 0;
+            initCards();
         }
     }
     deinitSystem();
@@ -264,15 +360,23 @@ void playGame() {
 
 void showCards() {
     initCards();
-    for (int i = 0; i < 56; i++) {
+    int x = 0;
+    for (int i = 0; i < 52; i++) {
         printf("%c%c ", cards[i].value, cards[i].suite);
+        if (++x >= 13)
+        {
+            printf("\n");
+            x = 0;
+        }
     }
+    printf("\n");
 }
+
 int main()
 {
     //showCards();
     playGame();
     printf("Thanks for playing!\n");
-    printf("https://8BitCoder.com\n");
+    printf("https://8BitCoder.com\n\n");
 	return 0;
 }
