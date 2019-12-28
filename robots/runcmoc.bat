1599d7e27a0e0c00d5f3acd26bbda79609ed163b
Abathur8bit <Abathur8bit@8bitcoder.com>
2019-12-27 22:15:57 -0500

    removed files that were not suppose to be checked in

--- robots/runcmoc.bat
+++
@@ -1,3 +0,0 @@
-pscp robots.c lee@titan:workspace/robots
-plink -batch lee@titan "cd workspace/robots && cmoc robots.c && writecocofile robots.dsk robots.bin"
-pscp lee@titan:workspace/robots/robots.dsk .
