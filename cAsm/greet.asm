feb81fe4eb6ceb73fa5fc7dc3c6c49fdd7b5060f
abathur8bit <abathur8bit@8bitcoder.com>
2022-07-27 01:31:49 -0400

    added greet back in so things compile again

---
+++ cAsm/greet.asm
@@ -0,0 +1,16 @@
+_greet		EXPORT
+_addNum		EXPORT
+
+		SECTION 	code
+_greet		ldd	#42
+		rts
+
+num		equ	4
+_addNum		pshs	u
+		leau	,s
+		ldd	num,u
+		addd	num,u
+		leas	,u
+		puls	u,pc
+		
+		ENDSECTION
