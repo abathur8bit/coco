252e733944162eb95dfe68e26b40ae23e0ef1385
Abathur8bit <Abathur8bit@8bitcoder.com>
2024-12-22 22:32:50 -0600

    - spriteasm.lua now shows sprite name instead of just 'sprite', and shows the filename - Using spaces instead of tabs - Updated ball to be smaller, indexed colors.

--- libs/pmode/spriteasm.lua
+++ libs/pmode/spriteasm.lua
@@ -33,41 +33,43 @@ function showPixels(w,h,p)
  end
 end 
 
-function fcb(w,h,p,f)
+function fcb(name,w,h,p,f)
  local row=""
  local bitPairs=0
- print("sprite."..f)
+ s=string.format("%s.%d",name,f)
+ print(s)
  for y=0,h-1 do
   binary=""
   for x=0,w-1 do
   n=y*w+x
-   if     p[n] == 0 then binary=binary.."00" 
-   elseif p[n] == 1 then binary=binary.."01" 
-   elseif p[n] == 2 then binary=binary.."10" 
-   elseif p[n] == 3 then binary=binary.."11" 
+   if     p[n] == 0 then binary=binary.."00"
+   elseif p[n] == 1 then binary=binary.."01"
+   elseif p[n] == 2 then binary=binary.."10"
+   elseif p[n] == 3 then binary=binary.."11"
    else                  binary=binary.."11" end
    bitPairs=bitPairs+1
-   if(bitPairs==4) then 
+   if(bitPairs==4) then
     if string.len(row)>0 then
-     row=row.."," 
+     row=row..","
     end
     row=row.."%"..binary
     binary=""
     bitPairs=0
    end
   end
-  print("		fcb	"..row)
+  print("                fcb     "..row)
   row=""
  end
-end 
+end
 
-function showimage(cel)
+function showimage(name,cel)
 	local img = cel.image:clone()
 
 	local box=cel.bounds
 	local sw=cel.sprite.width
 	local sh=cel.sprite.height
-	print("* sprite."..cel.frameNumber.." cel x,y,w,h: "..box.x..","..box.y..","..box.width..","..box.height)
+	--print("* sprite."..cel.frameNumber.." Aseprite cell info: x,y,w,h: "..box.x..","..box.y..","..box.width..","..box.height)
+	--print("* sprite."..cel.frameNumber.." frame data")
 	local pixels={}
 	for i=0,sw*sh do pixels[i]=0 end
 	c=0
@@ -81,19 +83,23 @@ function showimage(cel)
 			--print(it.x,it.y,px,py,pix)
 		--end
 	end
- 
+
 	--showPixels(sw,sh,pixels)
-	fcb(sw,sh,pixels,cel.frameNumber)
+	fcb(name,sw,sh,pixels,cel.frameNumber)
 end
 
 function header(sprite)
- print("sprite		fcb	"..sprite.width..","..sprite.height)
+ name=app.fs.fileTitle(sprite.filename)
+ print(string.format("; Sprite   : %s",name))
+ print(string.format("; Filename : %s",sprite.filename))
+ s=string.format("%-15s fcb     $%02X,$%02X   ; (%d,%d) width,height of all frames",name,sprite.width,sprite.height,sprite.width,sprite.height)
+ print(s)
 end
 
 local sprite=app.activeSprite
 for i,sprite in ipairs(app.sprites) do
   header(sprite)
   for i,cel in ipairs(sprite.cels) do
-    showimage(cel)
+    showimage(name,cel)
   end
 end
